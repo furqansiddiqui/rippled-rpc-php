e90ed96db0a71e8c47309c0acee7b08ae3df8031
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-08-20 04:09:19 +0500

    Exceptions

---
+++ src/Exception/RippledRPCException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is a part of "furqansiddiqui/rippled-rpc-php" package.
+ * https://github.com/furqansiddiqui/rippled-rpc-php
+ *
+ * Copyright (c) 2019 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/furqansiddiqui/rippled-rpc-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\Rippled\Exception;
+
+/**
+ * Class RippledRPCException
+ * @package FurqanSiddiqui\Rippled\Exception
+ */
+class RippledRPCException extends \Exception
+{
+}
